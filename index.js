8e69a7dd13dc94743b7043be4c3e9a6a9dce5c52
PrincGit <innocentemeka357@gmail.com>
2023-01-23 01:05:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var num1 =parseInt(prompt("enter first number"));
+var num2=parseInt(prompt("enter second number"));
+
+var add= num1+num2;
+output= num1 + "+" + num2 + "="+ add
+alert(output)
+
+var fname = prompt("Enter first name")
+var sname = prompt("Enter second name")
+
+var first4 = fname.slice(0,4);
+var first = sname.slice(0,1);
+
+var addd = first4 + first;
+
+var userid= addd.toLowerCase();
+
